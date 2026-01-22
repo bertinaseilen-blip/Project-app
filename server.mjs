23f1039d63ccac723613604fc3828cc34287dd4c
bertinaseilen-blip <bertinaseilen@gmail.com>
2026-01-22 12:15:43 +0100

    modified server.mjs

--- server.mjs
+++ server.mjs
@@ -1,15 +1,28 @@
 import express from "express";
 import middlewareRoute from "./routes/middlewareRoute.mjs";
-import { middlewareTask } from "./modules/middlewareTask.mjs";
+
 const PORT = 8080;
 const app = express();
 
 app.use(express.json());
-
-
-// Mount task routes
 app.use(middlewareRoute);
+
+app.get("/test", async (req, res) => {
+    await fetch("http://localhost:8080/task", {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            taskName: "Task 1",
+            completed: true
+        })
+    });
+
+    res.send("Test triggered");
+});
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+
