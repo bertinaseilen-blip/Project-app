6abd93446d5747086648401c0cb36f6fc9414d65
bertinaseilen-blip <bertinaseilen@gmail.com>
2026-03-02 16:36:06 +0100

    added new folder

---
+++ modules/storageProviders/storageProviderSQL.mjs
@@ -0,0 +1,38 @@
+import pkg from "pg";
+const { Pool } = pkg;
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+// -------------------------
+
+export async function createUser(id, username, consent) {
+  const result = await pool.query(
+    "INSERT INTO users (id, username, consent) VALUES ($1, $2, $3) RETURNING *",
+    [id, username, consent]
+  );
+
+  return result.rows[0];
+}
+
+// -------------------------
+
+export async function getUsers() {
+  const result = await pool.query("SELECT * FROM users");
+  return result.rows;
+}
+
+// -------------------------
+
+export async function deleteUser(id) {
+  const result = await pool.query(
+    "DELETE FROM users WHERE id = $1",
+    [id]
+  );
+
+  return result.rowCount > 0;
+}
